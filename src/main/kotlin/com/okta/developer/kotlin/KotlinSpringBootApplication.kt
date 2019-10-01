b5ad94af0353c9e0c17220598350bfb517546932
indiepopart <jimena@gmail.com>
2019-09-30 23:57:28 -0300

    initial commit

---
+++ src/main/kotlin/com/okta/developer/kotlin/KotlinSpringBootApplication.kt
@@ -0,0 +1,23 @@
+package com.okta.developer.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class KotlinSpringBootApplication {
+
+	fun main(args: Array<String>) {
+		runApplication<KotlinSpringBootApplication>(*args)
+	}
+
+	@GetMapping("/")
+	fun hello(@AuthenticationPrincipal authenticationToken: OAuth2AuthenticationToken): String {
+		return "Welcome, " + authenticationToken.principal.attributes["name"]
+	}
+}
+
